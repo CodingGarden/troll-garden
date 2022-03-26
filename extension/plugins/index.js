17667ba7c53806be3187ba255915b21f95721c11
omar2205 <omar2205a@yahoo.com>
2022-03-26 12:41:09 +0200

    add accept pies dialog to all website

--- extension/plugins/index.js
+++ extension/plugins/index.js
@@ -2,10 +2,12 @@ import RandomDiscordNotification from './background/RandomDiscordNotification.js
 import RandomSlackNotification from './background/RandomSlackNotification.js';
 import AllLinksRickRoll from './content/AllLinksRickRoll.js';
 import PageBlur from './content/PageBlur.js';
+import AcceptOurPies from './content/AcceptOurPies.js';
 
 export default [
   new RandomDiscordNotification(),
   new RandomSlackNotification(),
   new AllLinksRickRoll(),
   new PageBlur(),
+  new AcceptOurPies(),
 ];
