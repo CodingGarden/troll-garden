f04b645b6aeb5934e128fad9aa1a2e0c6f81c425
omar2205 <omar2205a@yahoo.com>
2022-03-28 23:51:30 +0200

    add every plugin id to index.js

--- extension/plugins/index.js
+++ extension/plugins/index.js
@@ -1,13 +1,13 @@
-import RandomDiscordNotification from './background/RandomDiscordNotification.js';
-import RandomSlackNotification from './background/RandomSlackNotification.js';
-import AllLinksRickRoll from './content/AllLinksRickRoll.js';
-import DeadPixel from './content/DeadPixel.js';
-import ComicSans from './content/ComicSans.js';
-import TextHighlight from './content/TextHighlight.js';
-import PageBlur from './content/PageBlur.js';
-import SlowYTVids from './content/SlowYTVids.js';
-import AcceptOurPies from './content/AcceptOurPies.js';
-import RedirectGoogleToBing from './background/RedirectGoogleToBing.js';
+import RandomDiscordNotification from './background/RandomDiscordNotification.js'; // id 0
+import RandomSlackNotification from './background/RandomSlackNotification.js'; // id 1
+import AllLinksRickRoll from './content/AllLinksRickRoll.js'; // id 2
+import DeadPixel from './content/DeadPixel.js'; // id 59
+import ComicSans from './content/ComicSans.js'; // id 58
+import TextHighlight from './content/TextHighlight.js'; // id 5
+import PageBlur from './content/PageBlur.js'; // id 51
+import SlowYTVids from './content/SlowYTVids.js'; // id 57
+import AcceptOurPies from './content/AcceptOurPies.js'; // id 60
+import RedirectGoogleToBing from './background/RedirectGoogleToBing.js'; // id 6
 
 export default [
   new RandomDiscordNotification(),
