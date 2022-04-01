02c288fcf3ed7156a903c3656c0b2c9db0cd5281
w3cj <cj@null.computer>
2022-04-01 12:51:47 -0600

    :cop: lint files

--- extension/plugins/content/KeyRepeat.js
+++ extension/plugins/content/KeyRepeat.js
@@ -12,20 +12,20 @@ class KeyRepeat extends ContentPlugin {
   };
 
   run() {
-    document.addEventListener("keydown", (e) => {
-        if ((e.key >= "a" && e.key <= "z") || (e.key >= "0" && e.key <= "9")
-         || (e.key >= "A" && e.key <= "Z")) {
-            const randomValue = Math.random();
-            if (randomValue < 0.1) { // Happens 10% of the time.
-                const element = e.target;
-                element.value = element.value + e.key;
-            }
+    document.addEventListener('keydown', (e) => {
+      if ((e.key >= 'a' && e.key <= 'z') || (e.key >= '0' && e.key <= '9')
+         || (e.key >= 'A' && e.key <= 'Z')) {
+        const randomValue = Math.random();
+        if (randomValue < 0.1) { // Happens 10% of the time.
+          const element = e.target;
+          element.value += e.key;
         }
+      }
     });
   }
 
   cleanup() {
-    
+
   }
 }
 
