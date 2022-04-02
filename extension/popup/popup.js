3bd38cc99b581e2f3b711d71f53f08cfe69eff19
tylerc97 <55768085+tylerc97@users.noreply.github.com>
2022-04-02 03:52:36 +0100

    Add date checker

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,34 +1,57 @@
-import plugins from '../plugins/index.js';
+import plugins from "../plugins/index.js";
 
-const contentElement = document.querySelector('#content-menu');
-const backgroundElement = document.querySelector('#background-menu');
+const contentElement = document.querySelector("#content-menu");
+const backgroundElement = document.querySelector("#background-menu");
 
-plugins.forEach((plugin) => {
-  const plugin_id = `plugin_${plugin.id}`;
-  chrome.storage.local.get(plugin_id, (savedSettings) => {
-    savedSettings = savedSettings[plugin_id] || plugin.settings;
-    const labelElement = document.createElement('label');
-    labelElement.setAttribute('for', plugin_id);
-    const inputElement = document.createElement('input');
-    inputElement.setAttribute('type', 'checkbox');
-    inputElement.setAttribute('id', plugin_id);
-    inputElement.setAttribute('name', plugin_id);
-    inputElement.setAttribute('role', 'switch');
-    inputElement.checked = savedSettings.enabled;
-    inputElement.addEventListener('input', () => {
-      savedSettings.enabled = inputElement.checked;
-      chrome.storage.local.set({
-        [plugin_id]: savedSettings,
+const checkDay = function () {
+  const today = new Date();
+  const todayMonth = today.getMonth();
+  const todayDay = today.getDate();
+  const aprilFool = new Date(2022, 3, 1);
+  const foolMonth = aprilFool.getMonth();
+  const foolDay = aprilFool.getDate();
+
+  return foolMonth === todayMonth && foolDay === todayDay;
+};
+
+if (checkDay()) {
+  plugins.forEach((plugin) => {
+    const plugin_id = `plugin_${plugin.id}`;
+    chrome.storage.local.get(plugin_id, (savedSettings) => {
+      savedSettings = savedSettings[plugin_id] || plugin.settings;
+      const labelElement = document.createElement("label");
+      labelElement.setAttribute("for", plugin_id);
+      const inputElement = document.createElement("input");
+      inputElement.setAttribute("type", "checkbox");
+      inputElement.setAttribute("id", plugin_id);
+      inputElement.setAttribute("name", plugin_id);
+      inputElement.setAttribute("role", "switch");
+      inputElement.checked = savedSettings.enabled;
+      inputElement.addEventListener("input", () => {
+        savedSettings.enabled = inputElement.checked;
+        chrome.storage.local.set({
+          [plugin_id]: savedSettings,
+        });
       });
+      const spanElement = document.createElement("span");
+      spanElement.textContent = plugin.name;
+      labelElement.append(inputElement);
+      labelElement.append(spanElement);
+      if (plugin.type === "background") {
+        backgroundElement.append(labelElement);
+      } else if (plugin.type === "content") {
+        contentElement.append(labelElement);
+      }
     });
-    const spanElement = document.createElement('span');
-    spanElement.textContent = plugin.name;
-    labelElement.append(inputElement);
-    labelElement.append(spanElement);
-    if (plugin.type === 'background') {
-      backgroundElement.append(labelElement);
-    } else if (plugin.type === 'content') {
-      contentElement.append(labelElement);
-    }
   });
-});
+} else {
+  document.body.innerHTML = "";
+  document.body.style.width = "500px";
+  document.body.style.height = "500px";
+  const header = document.createElement("h1");
+  header.textContent =
+    "CAN ONLY WORK ON APRIL FOOLS DAY!!! Come back then for more fun!";
+  header.style.textAlign = "center";
+  header.style.padding = "50px";
+  document.body.append(header);
+}
